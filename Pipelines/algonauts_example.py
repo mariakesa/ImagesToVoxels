00856ad368c305d5fb50b8f7255f3062eeee7b43
mariakesa <maria.kesa@gmail.com>
2023-01-28 12:40:52 +0200

    feature extraction

--- Pipelines/algonauts_example.py
+++ Pipelines/algonauts_example.py
@@ -1,4 +1,6 @@
-from ..utils import argObj, train_validation_partition_images, make_data_loaders, make_paths
+from ..utils import argObj, partition_indices, make_data_loaders, make_paths, feat_extr_pcs
+from torchvision.models.feature_extraction import get_graph_node_names
+import mlflow
 
 DATA_DIR='/home/maria/Algonauts2023'
 PARENT_SUBMISSION_DIR='/home/maria/Algonauts2023_submission'
@@ -8,9 +10,15 @@ def vanilla_alexnet_pipeline(subj, parent_submission_dir=PARENT_SUBMISSION_DIR, 
     data_dir='/home/maria/Algonauts2023'
     parent_submission_dir='/home/maria/Algonauts2023_submission'
     args = argObj(data_dir, parent_submission_dir, subj)
-    idxs_train, idxs_val, idxs_test=train_validation_partition_images(args, rand_seed=17)
+    idxs_train, idxs_val, idxs_test=partition_indices(args, rand_seed=17)
     train_imgs_paths, test_imgs_paths=make_paths(args.train_img_dir, args.test_img_dir)
     train_imgs_dataloader, val_imgs_dataloader, test_imgs_dataloader=make_data_loaders(train_imgs_paths, test_imgs_paths, batch_size=300)
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet')
+    train_nodes, _ = get_graph_node_names(model)
+    for layer in train_nodes[0:2]:
+        features_train, features_val, features_test=feat_extr_pcs(model,layer)
+        del model
+
 
 
 vanilla_alexnet_pipeline()
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from nilearn import datasets
 from nilearn import plotting
 import torch
 from torch.utils.data import DataLoader, Dataset
-from torchvision.models.feature_extraction import create_feature_extractor, get_graph_node_names
+from torchvision.models.feature_extraction import create_feature_extractor
 from torchvision import transforms
 from sklearn.decomposition import IncrementalPCA
 from sklearn.linear_model import LinearRegression
@@ -28,7 +28,7 @@ class argObj:
         if not os.path.isdir(self.subject_submission_dir):
             os.makedirs(self.subject_submission_dir)
 
-def load_fmri_for_visualization(args):
+def load_fmri(args):
     fmri_dir = os.path.join(args.data_dir, 'training_split', 'training_fmri')
     lh_fmri = np.load(os.path.join(fmri_dir, 'lh_training_fmri.npy'))
     rh_fmri = np.load(os.path.join(fmri_dir, 'rh_training_fmri.npy'))
@@ -48,7 +48,7 @@ def get_image_paths(args):
     return train_img_list, test_img_list, train_img_dir, test_img_dir
 
 
-def train_validation_partition_images(args,rand_seed=17):
+def partition_indices(args,rand_seed=17):
     train_img_list, test_img_list, train_img_dir, test_img_dir = get_image_paths(args)
     np.random.seed(rand_seed)
 
@@ -93,7 +93,7 @@ class ImageDataset(Dataset):
         img = Image.open(img_path).convert('RGB')
         # Preprocess the image and send it to the chosen device ('cpu' or 'cuda')
         if self.transform:
-            img = self.transform(img).to(device)
+            img = self.transform(img)
         return img
 
 
@@ -104,7 +104,7 @@ def make_paths(train_img_dir, test_img_dir):
 
     return train_imgs_paths, test_imgs_paths
 def make_data_loaders(train_imgs_paths, test_imgs_paths, batch_size=300):
-
+    idxs_train, idxs_val, idx_test = partition_indices(args, rand_seed=17)
     # The DataLoaders contain the ImageDataset class
     train_imgs_dataloader = DataLoader(
         ImageDataset(train_imgs_paths, idxs_train, transform),
@@ -120,5 +120,54 @@ def make_data_loaders(train_imgs_paths, test_imgs_paths, batch_size=300):
     )
 
     return train_imgs_dataloader, val_imgs_dataloader, test_imgs_dataloader
+
+
+def make_regr_targets(args,idxs_train,idxs_val):
+    idxs_train, idxs_val= partition_indices(args,rand_seed=17)
+    lh_fmri,rh_fmri=load_fmri(args)
+    lh_fmri_train = lh_fmri[idxs_train]
+    lh_fmri_val = lh_fmri[idxs_val]
+    rh_fmri_train = rh_fmri[idxs_train]
+    rh_fmri_val = rh_fmri[idxs_val]
+    return lh_fmri_train,rh_fmri_train,lh_fmri_val, rh_fmri_val
+
+def fit_pca(feature_extractor, dataloader):
+    batch_size=300
+    # Define PCA parameters
+    pca = IncrementalPCA(n_components=100, batch_size=batch_size)
+
+    # Fit PCA to batch
+    for _, d in tqdm(enumerate(dataloader), total=len(dataloader)):
+        # Extract features
+        ft = feature_extractor(d)
+        # Flatten the features
+        ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
+        # Fit PCA to batch
+        pca.partial_fit(ft.detach().cpu().numpy())
+    return pca
+
+
+def extract_features(feature_extractor, dataloader, pca):
+
+    features = []
+    for _, d in tqdm(enumerate(dataloader), total=len(dataloader)):
+        # Extract features
+        ft = feature_extractor(d)
+        # Flatten the features
+        ft = torch.hstack([torch.flatten(l, start_dim=1) for l in ft.values()])
+        # Apply PCA transform
+        ft = pca.transform(ft.cpu().detach().numpy())
+        features.append(ft)
+    return np.vstack(features)
+
+def feat_extr_pcs(model,layer, train_imgs_dataloader, val_imgs_dataloader, test_imgs_dataloader):
+    feature_extractor = create_feature_extractor(model, return_nodes=[layer])
+    pca = fit_pca(feature_extractor, train_imgs_dataloader)
+    features_train = extract_features(feature_extractor, train_imgs_dataloader, pca)
+    features_val = extract_features(feature_extractor, val_imgs_dataloader, pca)
+    features_test = extract_features(feature_extractor, test_imgs_dataloader, pca)
+    del pca
+    return features_train, features_val, features_test
+
 
 
